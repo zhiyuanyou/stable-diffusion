426f78bba35e564d69952f1d514744d23444e49c
zhiyuanyou <zhiyuanyou@foxmail.com>
2023-05-17 19:05:09 +0800

    feat(loss): revise loss from nll to l2

--- ldm/modules/losses/vae_loss.py
+++ ldm/modules/losses/vae_loss.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 
+from torch.nn import functional as F
+
 
 class VAELoss(nn.Module):
     def __init__(self, logvar_init=0.0, kl_weight=1.0):
@@ -19,26 +21,16 @@ class VAELoss(nn.Module):
                 split="train",
                 weights=None):
 
-        rec_loss = torch.abs(inputs.contiguous() -
-                             reconstructions.contiguous())
-
-        nll_loss = rec_loss / torch.exp(self.logvar) + self.logvar
-        weighted_nll_loss = nll_loss
-        if weights is not None:
-            weighted_nll_loss = weights * nll_loss
-        weighted_nll_loss = torch.sum(
-            weighted_nll_loss) / weighted_nll_loss.shape[0]
-        nll_loss = torch.sum(nll_loss) / nll_loss.shape[0]
+        rec_loss = F.mse_loss(reconstructions.contiguous(),
+                              inputs.contiguous())
         kl_loss = posteriors.kl()
         kl_loss = torch.sum(kl_loss) / kl_loss.shape[0]
-
-        loss = weighted_nll_loss + self.kl_weight * kl_loss
+        loss = rec_loss + self.kl_weight * kl_loss
 
         log = {
             "{}/total_loss".format(split): loss.clone().detach().mean(),
             "{}/logvar".format(split): self.logvar.detach(),
             "{}/kl_loss".format(split): kl_loss.detach().mean(),
-            "{}/nll_loss".format(split): nll_loss.detach().mean(),
             "{}/rec_loss".format(split): rec_loss.detach().mean(),
         }
         return loss, log
