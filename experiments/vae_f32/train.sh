760f80391e57a9d33653271dc0c30f399122d5b6
zhiyuanyou <zhiyuanyou@foxmail.com>
2023-05-30 07:14:57 +0000

    style(exp): revise default cfg

--- experiments/vae_f32/train.sh
+++ experiments/vae_f32/train.sh
@@ -1,3 +1,3 @@
-export PYTHONPATH=../../:$PYTHONPATH
-export CUDA_VISIBLE_DEVICES=$1
-python -u ../../main.py -b autoencoder_kl_8x8x64.yaml -t
+export PYTHONPATH=/opt/data/private/142/stable-diffusion/:$PYTHONPATH
+export CUDA_VISIBLE_DEVICES=0,1
+/root/anaconda3/envs/ldm/bin/python -u /opt/data/private/142/stable-diffusion/main.py -b autoencoder_kl_8x8x64.yaml -t > train.log
--- experiments/vae_f4/train.sh
+++
@@ -1,3 +0,0 @@
-export PYTHONPATH=../../:$PYTHONPATH
-export CUDA_VISIBLE_DEVICES=$1
-python -u ../../main.py -b autoencoder_kl_32x32x4.yaml -t
